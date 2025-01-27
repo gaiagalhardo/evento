3c3c392784992bf58986997d01f41da9d2f8f9ba
Gaia Galhardo <ti.gaiagalhardo@gmail.com>
2025-01-27 20:53:33 -0300

    configuracao inicial do projeto

---
+++ src/test/java/com/gaiagalhardo/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaiagalhardo.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
